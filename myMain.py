32d91628aa9fe1d4c1711adcb78cf1be12ae86a0
li00214 <33716712+li00214@users.noreply.github.com>
2020-03-08 16:14:49 +0800

    2020年3月8日更新数据和图表

--- myMain.py
+++ myMain.py
@@ -14,8 +14,8 @@ if __name__ == '__main__':
 #    get_province('ProvinceCode.csv','Wuhan-2019-nCoV.csv')
 #    get_country('CountryCode.csv','Wuhan-2019-nCoV.csv')   
     fileNameOfAll='Wuhan-2019-nCoV.csv'
-    dateTime='2020-03-07'
-    dateTitle='20191201-20200307'
+    dateTime='2020-03-08'
+    dateTitle='20191201-20200308'
     main_data_prepare(fileNameOfAll, dateTime)
     
     print('---------------------- Data Pre-processing End ---------------------')
